ccc2f8c500d06eb5b5c0a6539d68328184cb4a09
Do Ngoc Tri Cuong <dntcuong@gmail.com>
2014-10-22 23:48:08 +0700

    2014-10-22

--- Giai_The_Co_Tuong/src/com/kloesen/giaithe/MainActivity.java
+++ Giai_The_Co_Tuong/src/com/kloesen/giaithe/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kloesen.giaithe;
 
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -10,5 +12,14 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Intent intentSend = new Intent(MainActivity.this, MenuActivity.class);
+		startActivity(intentSend);
 	}
 }
