a8dc99c1948c8d9ce2e8d8726c1db7ef58c56a7c
Do Ngoc Tri Cuong <dntcuong@gmail.com>
2014-11-01 01:10:19 +0700

    20141101 0105

--- Giai_The_Co_Tuong/src/com/kloesen/giaithe/SplashScreenActivity.java
+++ Giai_The_Co_Tuong/src/com/kloesen/giaithe/SplashScreenActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class SplashScreenActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
                     sleep(5*1000);
                      
                     // After 5 seconds redirect to another intent
-                    Intent i=new Intent(getBaseContext(),MenuActivity.class);
+                    Intent i=new Intent(getBaseContext(), MainActivity.class);
                     startActivity(i);
                      
                     //Remove activity
