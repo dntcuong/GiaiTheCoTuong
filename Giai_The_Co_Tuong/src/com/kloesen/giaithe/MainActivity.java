6accd6e3165cd7f4b2e320968bd985f033cce2aa
Do Ngoc Tri Cuong <dntcuong@gmail.com>
2014-11-01 01:01:19 +0700

    20141101 0030

--- Giai_The_Co_Tuong/src/com/kloesen/giaithe/MainActivity.java
+++ Giai_The_Co_Tuong/src/com/kloesen/giaithe/MainActivity.java
@@ -1,45 +1,73 @@
 package com.kloesen.giaithe;
 
-
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	static int currentViewID = 0;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		Thread background = new Thread() {
-            public void run() {
-                 
-                try {
-                    // Thread will sleep for 5 seconds
-                    sleep(5*1000);
-                     
-                    // After 5 seconds redirect to another intent
-                    Intent i=new Intent(getBaseContext(),MenuActivity.class);
-                    startActivity(i);
-                     
-                    //Remove activity
-                    finish();
-                     
-                } catch (Exception e) {
-                 
-                }
-            }
-        };
-		
-        background.start();
-        
+	public void setContentView(int layoutResID) {
+		// TODO Auto-generated method stub
+		currentViewID = layoutResID;
+		super.setContentView(layoutResID);
 	}
 	
 	@Override
-    protected void onDestroy() {
-         
-        super.onDestroy();
-         
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainactivity_menu);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public void onBackPressed() {
+
+		switch (currentViewID) {
+		case R.layout.mainactivity_play:
+			setContentView(R.layout.mainactivity_menu);
+			break;
+
+		default:
+			super.onBackPressed();
+			break;
+		}
+		
+	}
+	
+	
+	public void onClickButton(View v) {
+
+		if (v.getId() == R.id.menuactivity_buttonPlay) {
+			v.setBackgroundResource(R.drawable.menuactivity_buttonplay_press);
+			setContentView(R.layout.mainactivity_play);
+	        v.setBackgroundResource(R.drawable.menuactivity_buttonplay);
+		}else{
+			Toast.makeText(this, "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
 }
---
+++ Giai_The_Co_Tuong/src/com/kloesen/giaithe/SplashScreenActivity.java
@@ -0,0 +1,45 @@
+package com.kloesen.giaithe;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splashscreen);
+		
+		Thread background = new Thread() {
+            public void run() {
+                 
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+                     
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),MenuActivity.class);
+                    startActivity(i);
+                     
+                    //Remove activity
+                    finish();
+                     
+                } catch (Exception e) {
+                 
+                }
+            }
+        };
+		
+        background.start();
+        
+	}
+	
+	@Override
+    protected void onDestroy() {
+         
+        super.onDestroy();
+         
+    }
+}
